4f255e0ed6d4a16c1e916c34788fa93a4b5d5510
naszi <naszi75@gmail.com>
2024-03-18 16:12:52 +0100

    Mortgage Calculator add elif statement app

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -6,7 +6,9 @@ if salary >= 2000:
     credit_score = int(input("What is your credit score? "))
     if credit_score > 800:
         print("Interest rate: 4%")
-    else:
+    elif credit_score > 750:
         print("Interest rate: 6%")
+    else:
+        print("Interest rate: 8%")
 else:
     print("Sorry, you are not eligible!")
