f42530452ce608ab5a4005395db9614008504702
naszi <naszi75@gmail.com>
2024-03-18 15:30:07 +0100

    Mortgage Calculator app

---
+++ mortgage_calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to Mortgage Calculator")
+salary = int(input("What is your salary? "))
+
+if salary >= 2000:
+    print("You are eligible for mortgage!")
+else:
+    print("Sorry, you are not eligible!")
