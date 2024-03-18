81ea370af30e3cc2c8db7d7e05092c59af67724a
naszi <naszi75@gmail.com>
2024-03-18 15:56:49 +0100

    Mortgage Calculator nest if statement app

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -3,5 +3,10 @@ salary = int(input("What is your salary? "))
 
 if salary >= 2000:
     print("You are eligible for mortgage!")
+    credit_score = int(input("What is your credit score? "))
+    if credit_score > 800:
+        print("Interest rate: 4%")
+    else:
+        print("Interest rate: 6%")
 else:
     print("Sorry, you are not eligible!")
